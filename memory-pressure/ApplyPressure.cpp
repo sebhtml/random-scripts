97fa644309c0a8a8245be67fef0739ef634a6c7f
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-11-01 11:20:59 -0400

    add tool to do some memory pressure
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

---
+++ memory-pressure/ApplyPressure.cpp
@@ -0,0 +1,63 @@
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(int count, char ** arguments) {
+
+	if(count != 2) {
+		cout << "Error: you must provide a number of bytes" << endl;
+		cout << endl;
+	}
+
+	int bytesInSlice = 2000000000;
+
+	vector<char*> pointers;
+
+	uint64_t totalBytes = 0;
+
+	istringstream input(arguments[1]);
+
+	input >> totalBytes;
+
+	uint64_t allocatedBytes = 0;
+
+	cout << "Bytes to allocate: " << totalBytes << endl;
+
+	while(allocatedBytes < totalBytes) {
+
+		int bytes = bytesInSlice;
+
+		char * memory = (char *) malloc(bytes * sizeof(char));
+
+		if(memory == NULL) {
+			cout << "Warning, could not allocate memory !" << endl;
+			break;
+		}
+
+		for(int j = 0 ; j < bytes ; ++j) {
+			memory[j] = 42;
+		}
+
+		pointers.push_back(memory);
+
+		allocatedBytes += bytes;
+
+		cout << "Bytes allocated so far: " << allocatedBytes << " / ";
+		cout << totalBytes << " (" << (0.0+allocatedBytes) / totalBytes * 100.00;
+		cout << "%)" << endl;
+	}
+
+	for(int i = 0 ; i < (int) pointers.size() ; ++i) {
+
+		free(pointers[i]);
+	}
+
+	pointers.clear();
+
+	return 0;
+}
---
+++ memory-pressure/README.md
@@ -0,0 +1,4 @@
+[root@ls28 cpu]# echo 5000 > /proc/sys/vm/vfs_cache_pressure
+
+
+
