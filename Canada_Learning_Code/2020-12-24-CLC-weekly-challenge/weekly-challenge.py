6ed01a34ba044966e5c4166679f222470398f448
Sebastien Boisvert <sebhtml@protonmail.com>
2021-01-16 12:07:48 -0500

    solve weekly challenge for 2020-12-24

--- Canada_Learning_Code/2020-12-24-CLC-weekly-challenge/weekly-challenge.py
+++ Canada_Learning_Code/2020-12-24-CLC-weekly-challenge/weekly-challenge.py
@@ -75,6 +75,160 @@ def isPalindrome(S: str):
 
     return True
 
+# Use Python's "PEP 8 -- Style Guide for Python Code"
+# https://www.python.org/dev/peps/pep-0008/
+
+def make_partition(sequence_S: str):
+    # Build a partition to recurse.
+    # sequence_S = <sequence_A> <sequence_B> <sequence_C>   (concatenation of strings)
+    #   with:
+    #     * <sequence_S> containing L   characters
+    #     * <sequence_A> containing 1   character
+    #     * <sequence_B> containing L-2 characters
+    #     * <sequence_C> containing 1   character
+    #
+    partition_subinterval_A_begin = 0
+    partition_subinterval_A_end   = partition_subinterval_A_begin + 1
+    partition_subinterval_B_begin = partition_subinterval_A_end
+    partition_subinterval_B_end   = len(sequence_S) - 1
+    partition_subinterval_C_begin = partition_subinterval_B_end
+    partition_subinterval_C_end   = partition_subinterval_C_begin + 1
+
+    sequence_A = sequence_S [ partition_subinterval_A_begin : partition_subinterval_A_end ]
+    sequence_B = sequence_S [ partition_subinterval_B_begin : partition_subinterval_B_end ]
+    sequence_C = sequence_S [ partition_subinterval_C_begin : partition_subinterval_C_end ]
+
+    return [sequence_A, sequence_B, sequence_C]
+
+def make_palindromic_super_sequence(sequence_S: str):
+    """
+    Find the length of the lexicographically-lowest shortest palindromic super sequence of an arbitrary sequence.
+    This is a Computer Science Ph.D. -level thing.
+    The name "minSupersequence" is not a good function name at all, in my opinion as a Ph.D. doctor.
+
+    # Proof
+    # state = ABCDBA
+    #   isPalindrome(ABCDBA) -> false
+    #   test ABCDBA
+    #        ^    ^  PASS
+    #   test ABCDBA
+    #         ^  ^   PASS
+    #   test ABCDBA
+    #          ^^    FAIL
+    #   lexicographically_sorted_branches = [ABCDCBA, ABDCDBA]
+    # state = ABCDCBA
+    #   isPalindrome(ABCDCBA) -> true
+    #   return length(ABCDCBA)
+
+    # Proof
+    # state = RACECARA
+    #   isPalindrome(RACECARA) -> false
+    #   test RACECARA
+    #        ^      ^    FAIL
+    #   lexicographically_sorted_branches = [ ARACECARA, RACECARAR ]
+    # state = ARACECARA
+    #   isPalindrome(ARACECARA) -> true
+    #   return length(ARACECARA)
+
+    # Proof
+    # state = RACECAR
+    #   isPalindrome(RACECAR) -> true
+    #   return length(RACECAR)
+    """
+
+    # By definition, we already have a super sequence.
+    # The only thing missing are some characters that makes it palindromic.
+
+    # Maybe the provided string is already correct.
+    if isPalindrome(sequence_S) is True:
+        return sequence_S
+
+    # Rely on isPalindrome for telling us what is palindromic and what is not.
+    # We don't need to know the definition of the mathematical properties of a palindromic sequence.
+
+    [sequence_A, sequence_B, sequence_C] = make_partition(sequence_S)
+
+    sequence_AC = sequence_A + sequence_C
+    sequence_B_is_palindromic = isPalindrome(sequence_B)
+    sequence_AC_is_palindromic = isPalindrome(sequence_AC)
+
+    if sequence_B_is_palindromic is True and sequence_AC_is_palindromic is True:
+        raise Exception("The logic of isPalindrome is incorrect.")
+
+    elif sequence_AC_is_palindromic is True:
+        # Logically, B is not palindromic, but AC is palindromic
+        # In that case, simply make the answer for B, and then concatenate A with answerB with C
+        answer_sequence_B = make_palindromic_super_sequence(sequence_B)
+        answer_ABC = sequence_A + answer_sequence_B + sequence_C
+        return answer_ABC
+
+    else:
+        # Since string AC is not palindromic, the following 2 branches need to be tried:
+        # * ABCA
+        # * CABC.
+        sequence_SA = sequence_S + sequence_A
+        sequence_CS = sequence_C + sequence_S
+        branches = [sequence_CS, sequence_SA]
+        branches.sort()
+
+        # Pick up the lexicographically lowest one.
+        answers = list(map(make_palindromic_super_sequence, branches))
+        lengths = list(map(len, answers))
+        index = lengths.index(min(lengths))
+        answer = answers[index]
+        return answer
+
+def lower_super_sequence(S: str):
+    """
+    Test the lexicographically-lowest shortest palindromic super sequence of an arbitrary sequence
+
+    >>> lower_super_sequence("RACECAR")
+    'RACECAR'
+
+    >>> lower_super_sequence("RACECARA")
+    'ARACECARA'
+
+    >>> lower_super_sequence("ABCDBA")
+    'ABCDCBA'
+
+    >>> lower_super_sequence("ABCDBE")
+    'AEBCDCBEA'
+
+    >>> lower_super_sequence("ABCDEF")
+    'ABCDEFEDCBA'
+
+    >>> lower_super_sequence("0123456789")
+    '0123456789876543210'
+
+    >>> lower_super_sequence("CLC")
+    'CLC'
+
+    >>> lower_super_sequence("Canada")
+    'CadanadaC'
+
+    >>> lower_super_sequence("Python")
+    'PnohtythonP'
+
+    >>> lower_super_sequence("AA")
+    'AA'
+
+    >>> lower_super_sequence("AB")
+    'ABA'
+
+    >>> lower_super_sequence("BA")
+    'ABA'
+
+    >>> lower_super_sequence("Z")
+    'Z'
+
+    >>> lower_super_sequence("")
+    ''
+    """
+
+    super_sequence = make_palindromic_super_sequence(S)
+    return super_sequence
+
+
 
 def minSupersequence(S: str):
     """
@@ -93,6 +247,9 @@ def minSupersequence(S: str):
     >>> minSupersequence("ABCDEF")
     11
     """
+
+    super_sequence = make_palindromic_super_sequence(S)
+    return len(super_sequence)
 
     pass  # remove this line and write your code below
 
